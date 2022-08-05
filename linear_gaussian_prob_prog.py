fixed posterior filtering covariance

linear_gaussian_prob_prog.py
@@ -302,8 +302,8 @@ class GaussianDistribution:
     @staticmethod
     def _condition_helper(dd, x_inds, z_inds, all_vars, z_values):
         # find indices which correspond to x_inds ( dimensionality might be > 1 )
-        x_dims = [len(var.mu) for var in np.array(all_vars)[x_inds]]
-        z_dims = [len(var.mu) for var in np.array(all_vars)[z_inds]]
+        x_dims = [len(var.mu) for var in np.array(all_vars)[x_inds.numpy()]]
+        z_dims = [len(var.mu) for var in np.array(all_vars)[z_inds.numpy()]]
 
         start_inds = [0]
         for var in all_vars[0:-1]:
@@ -335,7 +335,7 @@ class GaussianDistribution:
 
     def condition(self, z_rvs):
         z_inds = [self.left.index(var.r_var) for var in z_rvs]
-        all_vars = [var.r_var for var in self.left]
+        all_vars = [var for var in self.left]
         z_values = torch.tensor([var.value for var in z_rvs])
         x_inds = list(set(range(len(self.left))) - set(z_inds))
         z_inds = torch.tensor(z_inds)

main.py
@@ -1235,7 +1235,7 @@ if __name__ == "__main__":
 
     # epsilons = [-5e-3, 0.0, 5e-3]
     epsilons = [-5e-2, 0.0]
-    traj_lengths = torch.arange(10, 20, 1)
+    traj_lengths = torch.arange(2, 20, 1)
     dim = 1
     table = create_dimension_table(torch.tensor([dim]), random=False)
 
@@ -1260,4 +1260,4 @@ if __name__ == "__main__":
     # traj_length = 5
     # truth = trial_evidence(table, traj_length, dim)
 
-    # execute_filtering_posterior_convergence(table, traj_lengths, epsilons, dim)
+    execute_filtering_posterior_convergence(table, traj_lengths, epsilons, dim)